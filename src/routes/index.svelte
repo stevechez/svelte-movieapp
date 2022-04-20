d2578d964b49fc52abc9efd2b65ebb528d41ac97
Steve Maciaszek <stevem@Steves-MacBook-Pro-2.local>
2022-04-20 09:25:45 -0700

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=df131a256cf78ed76b7926454a2b1cb5`)
+        const data = await res.json();
+        if (res.ok) {
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte";
+    export let popular;
+</script>
+
+<section>
+    <PopularMovies {popular} />
+</section>
---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,61 @@
+<script context="module">
+    export async function load({ fetch, params }) {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=df131a256cf78ed76b7926454a2b1cb5`)
+        const movieDetail = await res.json();
+        if (res.ok) {
+            return {
+                props: { movieDetail }
+            }
+        }
+    }
+</script>
+
+<script>
+    export let movieDetail;
+</script>
+
+<div class="movie-details">
+    <div class="img-container">
+        <img src={'https://image.tmdb.org/t/p/original' + movieDetail.backdrop_path} alt={movieDetail.title} />
+    </div>
+    <div class="text-container">
+        <h1>
+            {movieDetail.title}
+        </h1>
+        <p class="overview">
+            {movieDetail.overview}
+        </p>
+        <p><span>Release Date: </span>
+        {movieDetail.release_date} <br />
+        <span>Budget: </span>
+        ${movieDetail.budget} <br />
+        <span>Rating: </span>
+        {movieDetail.vote_average} <br />
+        <span>Runtime: </span>
+        {movieDetail.runtime}mins <br />
+
+        </p>
+    </div>
+</div>
+
+<style>
+    h1 {
+        padding: 1rem 0rem 2rem;
+    }
+    p {
+        padding: 1rem 0rem;
+    }
+    .img-container {
+        width: 100%;
+    }
+    img {
+        width: 100%;
+        border-radius: 1rem;
+    }
+    .movie-details {
+        margin: 2rem 20%;
+    }
+    span {
+        font-weight: bold;
+    }
+</style>
